8c96e48a28e976c32a0c88c05f8fb2c7f828a3c0
zayn-opps <sojolsojol836@gmail.com>
2023-09-24 22:12:14 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+ function multify(num){
+console.log(`multiplication for ${num}`);
+for(let i=1; i<=10; i++){
+item = 1 * num;
+console.log(`${i} * ${num} =`, item)
+}
+
+ }
+ 
+ multify(1)
+ multify(2)
+ multify(3)
+ multify(4)
+ multify(5)
+ multify(6)
+ multify(7)
+ multify(8)
+ multify(9)
+ multify(10)
+ multify(11)
+ multify(12)
+ multify(13)
+ multify(14)
+ multify(15)
+ multify(16)
+ multify(17)
+ multify(18)
+ multify(19)
+ multify(20)
+ multify(21)
